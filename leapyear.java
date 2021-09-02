f055241b352c05b9b17b5d88c4670cd194f93405
newor121 <89961864+newor121@users.noreply.github.com>
2021-09-02 17:28:06 +0800

    Add files via upload
    basic calculator and triangle figure

---
+++ leapyear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class leapyear
+{ public static void main (String[] args){
+   int a;
+    Scanner inputNumScanner = new Scanner(System.in);
+    System.out.println("Enter a Year:");
+    a=inputNumScanner.nextInt();
+    if  (a%4==0)
+
+    System.out.println("leafyear");
+    else
+    
+    System.out.println("not leafyear");
+    
+}}
+    
+    
